b62691f7a3393eda1fef74ef89582d19ecb91d3c
FahimFoysalEmon <foysal@newroztech.com>
2022-10-11 18:17:06 +0600

    case statements PatternMatching.has_key?(%{:a => nil}, :a)

--- lib/pattern_matching.ex
+++ lib/pattern_matching.ex
@@ -1,7 +1,20 @@
 defmodule PatternMatching do
 
+  # 1! is 1 # => 1
   def factorial(1), do: 1
 
+  # 2! is 2 # => 2 * 1
+  # 3! is 6 # => 3 * 2 * 1
   def factorial(n) when n > 0, do: n*factorial(n-1)
+
+
+  def has_key?(map, key) do
+    case Map.get(map, key, :none) do
+      :none ->
+        false
+      _value ->
+        true
+    end
+  end
 
 end
